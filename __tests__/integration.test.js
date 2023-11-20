82fd8617ba4bde1a59efb11a117a245b0e480aec
hala_jash <starshiny567@gmail.com>
2023-11-20 16:41:36 +0000

    refract test to respond to feedback

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -16,8 +16,8 @@ describe('selectTopics()', () => {
       .then(({ body }) => {
         expect(body.topics).toHaveLength(3);
         expect(body.topics[0]).toMatchObject({
-          slug: 'mitch',
-          description: 'The man, the Mitch, the legend',
+          slug: expect.any(String),
+          description: expect.any(String),
         });
       });
   });
@@ -50,13 +50,13 @@ describe('/api/articles/:article_id', () => {
       .expect(200)
       .then(({ body }) => {
         expect(body.article).toMatchObject({
-          title: 'Sony Vaio; or, The Laptop',
-          topic: 'mitch',
-          author: 'icellusedkars',
-          body: 'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
-          created_at: "2020-10-16T05:03:00.000Z",
+          title: expect.any(String),
+          topic: expect.any(String),
+          author: expect.any(String),
+          body: expect.any(String),
+          created_at: expect.any(String),
           article_img_url:
-            'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700',
+          expect.any(String),
         });
       });
   });
--- controller/apiController.js
+++ controller/apiController.js
@@ -2,7 +2,6 @@ const { getApiModel } = require('../model/apiModel');
 
 exports.getApis = (req, res, next) => {
   getApiModel().then((dataApi) => {
-    console.log(dataApi)
     res.status(200).send({ dataApi });
   });
 };
