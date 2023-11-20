9bdd39f7ee22ebb618128fa307dc39e1688b2b51
hala_jash <starshiny567@gmail.com>
2023-11-20 11:58:40 +0000

    create endpoints to retrieve all topics

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 const express = require('express');
 const app = express();
-const {
-  handlePsqlErrors,
-  handleCustomErrors,
-  handle404,
-  handle500,
-  handleUnhandledErrors,
-} = require('./errorHandle');
+const { handle404 } = require('./errorHandle');
 const { getTopics } = require('./controller/topicController');
-
 
 app.use(express.json());
 
 app.get('/api/topics', getTopics);
 
-app.use(handlePsqlErrors);
-app.use(handleCustomErrors);
 app.use(handle404);
-app.use(handle500);
-app.use(handleUnhandledErrors);
-
 
 module.exports = app;
