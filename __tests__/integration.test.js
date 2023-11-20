85a987f9bc6b4e1a3ac8deb4bf290b263eaa74bf
hala_jash <starshiny567@gmail.com>
2023-11-20 14:25:08 +0000

    respond to feedback

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -13,8 +13,24 @@ describe('selectTopics()', () => {
       .get('/api/topics')
       .expect(200)
       .then(({ body }) => {
-        // console.log(body.topics);
-        expect(body.topics).toHaveLength(3);
+          expect(body.topics).toHaveLength(3);
+          expect(body.topics[0]).toMatchObject({
+            slug: 'mitch',
+            description: 'The man, the Mitch, the legend',
+          });
       });
   });
+    
 });
+
+describe('error handling()', () => {
+    test('returns an error with invalid endpoint', () => {
+      return request(app)
+        .get('/api/topic')
+        .expect(404)
+        .then(({ body }) => {
+            expect(body.msg).toBe('Not Found');
+        });
+    });
+      
+  });
--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const app = express();
 const { handle404 } = require('./errorHandle');
 const { getTopics } = require('./controller/topicController');
 
-app.use(express.json());
 
 app.get('/api/topics', getTopics);
 
-app.use(handle404);
+app.all('*',handle404)
 
 module.exports = app;
